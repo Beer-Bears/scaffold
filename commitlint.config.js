9a595204ef5d6f8074cab7c8a209489cd0fd6564
peplxx <91543105+peplxx@users.noreply.github.com>
2025-06-18 17:31:32 +0300

    chore: add conventional confgi

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'
+    ]],
+    'subject-case': [2, 'always', 'lower-case'],
+    'header-max-length': [2, 'always', 100],
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+  }
+};
