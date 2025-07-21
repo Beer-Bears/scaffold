a054ce61a10b811f14649b119b5f6eeb48637406
Dmitry Prosvirkin <115407430+Trunn5@users.noreply.github.com>
2025-07-21 22:37:32 +0300

    feat(mcp): add all classes, functions tool (#135)

--- src/mcp/server.py
+++ src/mcp/server.py
@@ -1,6 +1,10 @@
 from fastmcp import FastMCP
 
-from src.generator.generator import get_node_information
+from src.generator.generator import (
+    get_all_classes,
+    get_all_functions,
+    get_node_information,
+)
 
 mcp = FastMCP(
     name="Scaffold MCP",
@@ -24,4 +28,28 @@ def get_code_entity_information(
     """
     return {
         "response": get_node_information(entity_name),
+    }
+
+
+@mcp.tool
+def get_all_functions_nodes_names() -> dict:
+    """
+    Return all functions names and its path in project.
+
+    Better call this tool than use grep for search exact names
+    """
+    return {
+        "response": get_all_functions(),
+    }
+
+
+@mcp.tool
+def get_all_classes_nodes_names() -> dict:
+    """
+    Return all classes names and its path in project.
+
+    Better call this tool than use grep for search exact names
+    """
+    return {
+        "response": get_all_classes(),
     }
